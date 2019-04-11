1ce7a60e1c601ecdf370f9b4e060ebfc98fea6e6
reno <srasmus2@emich.edu>
2019-04-11 14:24:48 -0400

    Simple syntax fix

--- numenor.py
+++ numenor.py
@@ -14,38 +14,38 @@ def prog_parser() :
     parser.add_argument("op_name", help="Gives a name to the machines that are spun up in Digital Ocean.")
     parser.add_argument("-a", "--apply", help="Applies the infrastructure and spins up the machines.", action="store_true")
     parser.add_argument("-d", "--destroy", help="Destroys any current infrastructure that is spun up.", action="store_true")
-    parser.add_argument("--op1", help="Uses OP1 as target infrasture.", action="store_true")
-    parser.add_argument("--op2", help="Uses OP2 as target infrasture.", action="store_true")
-    parser.add_argument("--op3", help="Uses OP3 as target infrasture.", action="store_true")
+    parser.add_argument("--ops1", help="Uses OPS1 as target infrasture.", action="store_true")
+    parser.add_argument("--ops2", help="Uses OPS2 as target infrasture.", action="store_true")
+    parser.add_argument("--ops3", help="Uses OPS3 as target infrasture.", action="store_true")
     args = parser.parse_args()
     if args.apply :
         if args.op1 :
-            print "[!] Spinning up OP1"
-            os.chdir("/etc/terraform/op1")
+            print "[!] Spinning up OPS1"
+            os.chdir("/etc/terraform/ops1")
             print os.getcwd()
         elif args.op2 :
-            print "[!] Spinning up OP2!"
-            os.chdir("/etc/terraform/op2")
+            print "[!] Spinning up OPS2!"
+            os.chdir("/etc/terraform/ops2")
             print os.getcwd()
         elif args.op3 :
-            print "[!] Spinning up OP3!"
-            os.chdir("/etc/terraform/op3")
+            print "[!] Spinning up OPS3!"
+            os.chdir("/etc/terraform/ops3")
             print os.getcwd()
         else :
             print "[!!!] Please specify the infrastructure to apply!"
         print "[DEBUG] Infrastructure {} applied!".format(args.op_name)
     if args.destroy :
         if args.op1 : 
-            print "[!] Destroying OP1!"
-            os.chdir("/etc/terraform/op1")
+            print "[!] Destroying OPS1!"
+            os.chdir("/etc/terraform/ops1")
             print os.getcwd()
         elif args.op2 :
-            print "[!] Destroying OP2!"
-            os.chdir("/etc/terraform/op2")
+            print "[!] Destroying OPS2!"
+            os.chdir("/etc/terraform/ops2")
             print os.getcwd()
         elif args.op3 :
-            print "[!] Destroying OP3!"
-            os.chdir("/etc/terraform/op3")
+            print "[!] Destroying OPS3!"
+            os.chdir("/etc/terraform/ops3")
             print os.getcwd()
         else :
             print "[!!!] Please specify the infrastructure to destroy!"
