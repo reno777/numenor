70f5b6252682162267a73720e937fec0319c949f
reno <srasmus2@emich.edu>
2019-04-11 16:13:12 -0400

    Adds terraform functionality to numenor.py

--- numenor.py
+++ numenor.py
@@ -8,6 +8,7 @@
 #Below are all of the module imports
 import argparse
 import os
+import subprocess
 
 def prog_parser() :
     parser = argparse.ArgumentParser()
@@ -18,38 +19,51 @@ def prog_parser() :
     parser.add_argument("--ops2", help="Uses OPS2 as target infrasture.", action="store_true")
     parser.add_argument("--ops3", help="Uses OPS3 as target infrasture.", action="store_true")
     args = parser.parse_args()
+    terra = "terraform apply --auto-approve -var op_name={}".format(args.op_name)
     if args.apply :
         if args.ops1 :
             print "[!] Spinning up OPS1"
             os.chdir("/etc/terraform/ops1")
-            print os.getcwd()
+            print os.getcwd() #debug
+            subprocess.call(["terraform", "init"])
+            subprocess.call(args, shell=True)
         elif args.ops2 :
             print "[!] Spinning up OPS2!"
             os.chdir("/etc/terraform/ops2")
             print os.getcwd()
+            subprocess.call(["terraform", "init"])
+            subprocess.call(args, shell=True)
         elif args.ops3 :
             print "[!] Spinning up OPS3!"
             os.chdir("/etc/terraform/ops3")
             print os.getcwd()
+            subprocess.call(["terraform", "init"])
+            subprocess.call(args, shell=True)
         else :
             print "[!!!] Please specify the infrastructure to apply!"
-        print "[DEBUG] Infrastructure {} applied!".format(args.op_name)
+        print "[DEBUG] Infrastructure '{}' applied!".format(args.op_name)
     if args.destroy :
         if args.ops1 : 
             print "[!] Destroying OPS1!"
             os.chdir("/etc/terraform/ops1")
             print os.getcwd()
+            subprocess.call(["terraform", "init"])
+            subprocess.call(args, shell=True)
         elif args.ops2 :
             print "[!] Destroying OPS2!"
             os.chdir("/etc/terraform/ops2")
             print os.getcwd()
+            subprocess.call(["terraform", "init"])
+            subprocess.call(args, shell=True)
         elif args.ops3 :
             print "[!] Destroying OPS3!"
             os.chdir("/etc/terraform/ops3")
             print os.getcwd()
+            subprocess.call(["terraform", "init"])
+            subprocess.call(args, shell=True)
         else :
             print "[!!!] Please specify the infrastructure to destroy!"
-        print "[DEBUG] Infrastructure {} destroyed!".format(args.op_name)
+        print "[DEBUG] Infrastructure '{}' destroyed!".format(args.op_name)
 
 if __name__ == "__main__" :
     prog_parser()
