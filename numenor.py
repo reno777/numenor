286f2cd81956e250bb34013c525355d3dffd67be
reno <srasmus2@emich.edu>
2019-04-11 12:11:15 -0400

    Adds initial numenor script

---
+++ numenor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2.7
+
+"""
+### AUTHOR: 0xreno
+### LICENSE: GNU-GPL v3.0
+"""
+
+#Below are all of the module imports
+import os
+import sys
+import pickle
+
+if __name__ == "__main__" :
+    main()
